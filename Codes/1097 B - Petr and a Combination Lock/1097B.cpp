4612811879b65a81625650290791b9245f0b1531
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-06-10 04:02:48 +0600

    Add solution for problem 1097B - Petr and a Combination Lock

---
+++ Codes/1097 B - Petr and a Combination Lock/1097B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    
+    for (int mask = 0; mask < (1 << n); mask++) 
+    {
+        int sum = 0;
+        for (int i = 0; i < n; i++) 
+        {
+            if (mask & (1 << i)) sum -= v[i];
+            else sum += v[i];
+        }
+        
+        if (sum % 360 == 0) 
+        {
+            yn(1);
+            return;
+        }
+    }
+    yn(0);
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

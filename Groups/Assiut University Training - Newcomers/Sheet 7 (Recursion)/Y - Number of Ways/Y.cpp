eea2a526ef17cf624cc601912e188267e10a46d5
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-06-13 01:59:11 +0600

    Add solutions for palindrome array and number of ways problems

---
+++ Groups/Assiut University Training - Newcomers/Sheet 7 (Recursion)/Y - Number of Ways/Y.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+ll fun(ll s, ll e)
+{
+    if(s > e)   return 0;
+    if(s == e)  return 1;
+    ll ct1 = fun(s + 1, e);
+    ll ct2 = fun(s + 2, e);
+    ll ct3 = fun(s + 3, e);
+    return ct1 + ct2 + ct3;
+}
+
+void solve()
+{
+    ll s, e;
+    cin >> s >> e;
+    ll ans = fun(s, e);
+    cout << ans << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

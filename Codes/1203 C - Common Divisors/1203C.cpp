ac9a8e8354e841564a839245f12b7a715064f957
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-06-13 16:52:15 +0600

    added solution of 1203C

---
+++ Codes/1203 C - Common Divisors/1203C.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    ll n, g = 0, ans = 0;
+    cin >> n;
+    for(ll i = 0; i < n; ++i)
+    {
+        ll x;
+        cin >> x;
+        g = gcd(g, x);
+    }
+
+    for(ll i = 1; i * i <= g; ++i)
+    {
+        if(g % i == 0)
+        {
+            ans++;
+            if(i != g / i) ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

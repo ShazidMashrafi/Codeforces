366d8130d3c33824d971920724881f40a288e902
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-06-17 03:33:04 +0600

    Add solution for problem 2048 E - Kevin and Bipartite Graph

---
+++ Codes/2048 E - Kevin and Bipartite Graph/2048E.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n, m;
+    cin >>  n >> m;
+    if(m > 2 * n - 1)
+    {
+        yn(0);
+        return;
+    }
+    yn(1);
+    for(int i = 0; i < 2 * n; ++i)
+    {
+        for(int j = 0; j < m; ++j)
+        {
+            int color = ((i+j) % (2*n)) / 2 + 1;
+            cout << color << (j == m-1 ? "" : " ");
+        }
+        cout << endl;
+    }
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

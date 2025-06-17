5d855a22a50ddc5382f613218c8b1fa9e05d100a
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-06-18 04:19:59 +0600

    Add solution for problem 2026 C - Action Figures

---
+++ Codes/2026 C - Action Figures/2026C.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    ll n, ans = 0;
+    string s;
+    cin >> n >> s;
+    set<ll>free, pay;
+    for(int i = 0; i < n; ++i)
+    {
+        ans += i + 1;
+        if(s[i] == '0')
+            pay.insert(i + 1);
+        else
+        {
+            if(!pay.empty()) 
+            {
+                pay.erase(pay.begin());
+                free.insert(i + 1);
+            }
+            else if(!free.empty())
+            {
+                pay.insert(*free.begin());
+                free.erase(free.begin());
+                free.insert(i + 1);
+            }
+            else
+                pay.insert(i + 1);
+        }
+    }
+    for(auto &x : free)
+        ans -= x;
+    cout << ans << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

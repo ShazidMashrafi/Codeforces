3fd7069a3e3e9c6e29dd459daa73436c24e2f5b1
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-06-22 01:04:17 +0600

    Add solution for problem 1579 E1 - Permutation Minimization by Deque

---
+++ Codes/1579 E1 - Permutation Minimization by Deque/1579E1.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    deque<ll>d;
+    for(ll i = 0; i < n; ++i)
+    {
+        int x;
+        cin >> x;
+        if(!d.empty() && d.front() > x)
+            d.push_front(x);
+        else
+            d.push_back(x);
+    }
+    for(auto &x : d)    cout << x << " ";
+    cout << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

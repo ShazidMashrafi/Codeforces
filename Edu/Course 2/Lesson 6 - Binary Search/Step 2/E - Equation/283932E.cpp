ca854dfb4fb94d380fd2e0fc5a180d6e08acb00d
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-07-03 02:10:24 +0600

    implement solution for problem E - Equation

---
+++ Edu/Course 2/Lesson 6 - Binary Search/Step 2/E - Equation/283932E.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    long double c;
+    cin >> c;
+    long double low = 0.0, high = 1e10, ans = 0.0, mid, val;
+    for(int tt = 0; tt < 200; ++tt)
+    {
+        mid = (low + high) / 2.00;
+        val = mid * mid + sqrt(mid);
+        if(val < c)
+        {    
+            low = mid;
+        }
+        else
+        {
+            ans = mid;
+            high = mid;
+        }
+    }
+    cout << Dpos(15) << ans << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

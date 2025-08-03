fc3c1fdc7c61e5b6fc5026d8b783c906bc5985d2
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-08-04 02:14:37 +0600

    Add solution for problem 1201 C - Maximum Median

---
+++ Codes/1201 C - Maximum Median/1201C.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll>v(n);
+    for(auto &i : v) cin >> i;
+    sort(all(v));
+    ll low = 0, high = 1e12, ans = 0;
+    while(low <= high)
+    {
+        ll mid = low + (high - low) / 2;
+        ll ct = 0;
+        for(int i = n / 2; i < n; ++i)
+        {
+            ct += max(0ll, mid - v[i]);
+        }
+        if(ct <= k)
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

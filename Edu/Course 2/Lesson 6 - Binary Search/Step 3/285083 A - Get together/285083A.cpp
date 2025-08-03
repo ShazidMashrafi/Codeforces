14eac0199ad257daf5047d1772b2da1c2c8cd074
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-08-04 03:05:01 +0600

    Add solution for problem 285083 A - Get together

---
+++ Edu/Course 2/Lesson 6 - Binary Search/Step 3/285083 A - Get together/285083A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<ll>x(n), v(n);
+    for(int i = 0; i < n; ++i)
+        cin >> x[i] >> v[i];
+    double low = 0, high = 1e18, ans = 1e18;
+    for(int itr = 0; itr < 100; ++itr)
+    {
+        double mid = (low + high) / 2;
+        double l = -1e20, r = 1e20;
+        for(int i = 0; i < n; ++i)
+        {
+            l = max(l, x[i] - v[i] * mid);
+            r = min(r, x[i] + v[i] * mid);
+        }
+        if(l <= r)
+        {
+            ans = mid;
+            high = mid;
+        }
+        else
+            low = mid;
+    }
+    cout << Dpos(10) << ans << endl;
+
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

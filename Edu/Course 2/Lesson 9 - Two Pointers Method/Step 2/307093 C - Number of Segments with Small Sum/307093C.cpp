5812d1e026b1bc649c0a4aaeb99e6af15c429289
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-08-28 01:58:05 +0600

    Add solution for problem 307093 C - Number of Segments with Small Sum

---
+++ Edu/Course 2/Lesson 9 - Two Pointers Method/Step 2/307093 C - Number of Segments with Small Sum/307093C.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    ll n, s;
+    cin >> n >> s;
+    vector<int> a(n);
+    for(auto &i : a)    cin >> i;
+
+    ll l = 0, x = 0, res = 0;
+    for(ll r = 0; r < n; r++) {
+        x += a[r];
+        while(x > s) {
+            x -= a[l];
+            l++;
+        }
+        res += r - l + 1;
+    }
+    cout << res << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

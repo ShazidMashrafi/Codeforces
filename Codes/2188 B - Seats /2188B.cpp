2f9256fab510372980a68854ca328326c548b62d
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2026-01-30 04:07:52 +0600

    Add solutions for Codeforces 2188 A, B

---
+++ Codes/2188 B - Seats /2188B.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+    s = "1" + s + "1";
+    int ans = 0;
+    for(int i = 1, l = 0; i <= n; ++i) {
+        if(s[i] == '1') {
+            ans++;
+            continue;
+        }
+        if(s[i - 1] == '1') {
+            l = i;
+        }
+        if(s[i + 1] == '1') {
+            int c = (l == 1) + (i == n);
+            ans += (i - l + 1 + c) / 3;
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

9d07420313e1bf014a18dd7f12b13ccf2f20f912
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2026-08-16 11:54:54 +0600

    added 1374C

---
+++ GYMS/GYM 106179/106179 B - Pseudo Palindrome/106179B.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#if defined(LOCAL) && !defined(ONLINE_JUDGE)
+#include "debug.h"
+#else
+#define dbg(...)
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n, d;
+    cin >> n >> d;
+    vector<int>v(n);
+    for(auto &i : v)    cin >> i;
+    sort(all(v));
+
+    int ct = 0;
+    for(int i = 0; i < n - 1; i +=2) {
+        if(v[i + 1] - v[i] > d) {
+            ct++;
+            i--;
+        }
+    }
+    if(n & 1) {
+        ct--;
+    }
+    yn(ct <= 0);
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

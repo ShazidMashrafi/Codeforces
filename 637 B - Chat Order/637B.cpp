9908f1faa65354e7fb6f86749b22ffd2b9e2c292
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2026-08-31 11:31:32 +0600

    added 637b and 659A

---
+++ 637 B - Chat Order/637B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#if defined(LOCAL) && !defined(ONLINE_JUDGE)
+#include <debug.h>
+#else
+#define dbg(...)
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<string> a(n);
+    for (auto &x : a) cin >> x;
+
+    unordered_map<string, bool> mp;
+    vector<string> ans;
+
+    for (int i = n - 1; i >= 0; --i)
+    {
+        if (!mp[a[i]])
+        {
+            ans.push_back(a[i]);
+            mp[a[i]] = true;
+        }
+    }
+    
+    for(auto x : ans) {
+        cout << x << endl;
+    }
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}

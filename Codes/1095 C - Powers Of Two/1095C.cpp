a89311913c6b5ff5c2847839fd34e5e0a6699558
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2026-09-12 13:39:41 +0600

    add solution for Codeforces 1095C Powers Of Two

---
+++ Codes/1095 C - Powers Of Two/1095C.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#if defined(LOCAL) && !defined(ONLINE_JUDGE)
+#include <debug.h>
+#else
+#define dbg(...)
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    int cnt[31] = {0};
+    int total = 0;
+    for(int i = 0; i < 31; ++i) {
+        if((n >> i) & 1) {
+            cnt[i] = 1;
+            total++;
+        }
+    }
+    dbg(cnt);
+
+    if(k < total || k > n) {
+        yn(0);
+        return;
+    }
+
+    for(int i = 30; i > 0; --i) {
+        if(total < k) {
+            int take = min(cnt[i], k - total);
+            cnt[i] -= take;
+            cnt[i - 1] += 2 * take;
+            total += take;
+        }
+    }
+
+    yn(1);
+    for(int i = 0; i < 31; ++i) {
+        while (cnt[i] > 0) {
+            cout << (1 << i) << " ";
+            cnt[i]--;
+        }
+    }
+    cout << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}
